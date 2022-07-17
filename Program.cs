09c224d9549902063e8e20b30ae7ef9f577da42f
Brian Franco <Brian Franco@DESKTOP-LK72KHB>
2022-07-17 00:34:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using TradingEngineServer.core;
+
+
+
---
+++ TradingEngineServer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.core
+{
+    class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {
+        private readonly ILogger<TradingEngineServer> _logger;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger, IOptions<>)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested) 
+            { 
+
+            }
+        }
+    }
+}
+
+
